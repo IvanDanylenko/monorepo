17e389d0d1005402d8c332a55b0d5c8a2b2cc500
Ivan Danylenko <danylenko.ivan11@gmail.com>
2022-06-02 18:43:57 +0300

    Rename woordhunt-chinese into woordhunt

--- apps/woordhunt/jest.config.ts
+++ apps/woordhunt/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'woordhunt-chinese',
+  displayName: 'woordhunt',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/woordhunt-chinese',
+  coverageDirectory: '../../coverage/apps/woordhunt',
 };
