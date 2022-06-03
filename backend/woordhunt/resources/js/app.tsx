617ed1d53e17a6f2c2d3b1b2ebb54b80e5a6160f
Ivan Danylenko <danylenko.ivan11@gmail.com>
2022-06-03 10:11:31 +0300

    Remove laravel-mix configuration

--- backend/woordhunt/resources/js/app.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/inertia-react';
-
-createInertiaApp({
-  resolve: (name) => require(`./pages/${name}`),
-  setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />);
-  },
-});
